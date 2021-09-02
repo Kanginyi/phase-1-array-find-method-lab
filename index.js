1b9e59e06525f274a6579c1aba2ccc8c078433e5
Eric Yi <erickanginyi@gmail.com>
2021-09-02 14:28:49 -0400

    commit tree edd97936d79661af850ac25276d8d5cc4678dd49

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-
+const superbowlWin = (array) => {
+    const check = array.find(element => element.result === "W");
+    if (check === undefined) {
+      return undefined;
+    } else {
+      return check.year;
+    }
+  }
